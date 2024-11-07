145670064359a1865f14aa823014277f2b078584
Silvan Zahno <silvan.zahno@hevs.ch>
2024-11-07 09:41:47 +0000

    2024-11-07 09:41:44: Automatic Library Update with deployLibs.bash :shipit:

--- Libs/UVM/hdl/driverControl_sim.vhd
+++ Libs/UVM/hdl/driverControl_sim.vhd
@@ -1,11 +1,16 @@
 LIBRARY std;
   USE std.textio.all;
+  use std.env.stop;
+
 LIBRARY Common_test;
   USE Common_test.testUtils.all;
 
 ARCHITECTURE RTL OF driverControl IS
 
-  signal testInfo: string(driverTransaction'range) := (others => ' ');
+  signal info : string(driverTransaction'range)
+    := pad("", driverTransaction'length);
+  signal target : string(driverTransaction'range)
+    := pad("", driverTransaction'length);
 
 BEGIN
   ------------------------------------------------------------------------------
@@ -14,14 +19,14 @@ BEGIN
     file driverFile : text;
     variable driverLine : line;
     variable preComment : line;
-    variable severityLevel_line : line;
-    variable severityLevel : severity_level;
     variable command : line;
     variable delay : time;
     variable address : line;
+    variable skipcommands : std_ulogic := '0';
+
   begin
-    driverTransaction <= pad("idle", driverTransaction'length);
-    if verbosity > 0 then
+    if verbosity > 2 then
+      driverTransaction <= pad("idle", driverTransaction'length);
       print("Opening driver sequence file " & driverFileSpec);
     end if;
 
@@ -32,72 +37,73 @@ BEGIN
       driverLine := preComment;
       trim_line(driverLine);
       if driverLine.all'length > 0 then
-        --lc(driverLine);
-        if verbosity > 1 then
+        if verbosity > 2 then
           print(driverLine.all);
         end if;
         read_first(driverLine, command);
-        if verbosity > 1 then
-          print(command.all & ":" & driverLine.all);
-        end if;
-                                               -- update test information string
-        if command.all = "info" then
-          if verbosity > 0 then
-            print("Info: " & driverLine.all);
-          end if;
-          testInfo <= pad(driverLine.all, testInfo'length);
-                                                  -- advance to a specified time
-        elsif command.all = "at" then
-          if verbosity > 0 then
-            print(cr & "Advancing simulation to time " & driverLine.all);
-          end if;
-          delay := sscanf(driverLine.all);
-          wait for delay - now;
-                                               -- advance with a given time step
-        elsif command.all = "wait" then
-          if verbosity > 0 then
-            print(cr &"Advancing simulation of step " & driverLine.all);
-          end if;
-          delay := sscanf(driverLine.all);
-          wait for delay;
-                                                     -- simulate false assertion
-        elsif command.all = "assert" then
-          read_first(driverLine, severityLevel_line);
-          if severityLevel_line.all = "note" then
-            severityLevel := note;
-          elsif severityLevel_line.all = "warning" then
-            severityLevel := warning;
-          elsif severityLevel_line.all = "error" then
-            severityLevel := error;
-          else
-            severityLevel := failure;
-          end if;
-          assert false
-            report driverLine.all
-            severity severityLevel;
         
-    		elsif command.all = "end_sim" then
-          if verbosity > 0 then
-            print(cr & cr & "================================");
-            print("================================");
-            print("Simulation end");
-            print("================================");
-            print("================================");
+        if command.all = "skip" then
+          skipcommands := '1';
+        elsif command.all = "endskip" then
+          skipcommands := '0';
+        elsif skipcommands = '0' then
+          if verbosity > 2 then
+            print(command.all & ":" & driverLine.all);
           end if;
-    			exit;
-		    else
-          driverTransaction <= pad(" ", driverTransaction'length);
-          wait for 0 ns;
-          driverTransaction <= pad(
-            command.all & " " & driverLine.all,
-            driverTransaction'length
-          );
-          if verbosity > 0 then
-            print(command.all & " " & driverLine.all);
+          if command.all = "info" then
+            if verbosity > 0 then
+              print("================================" & cr & cr & "================================");
+              print("Step: " & driverLine.all);
+              print("Began at: " & sprintf("%tn", now));
+              info <= pad(driverLine.all, driverTransaction'length);
+            end if;
+          
+          elsif command.all = "target" then
+            target <= pad(driverLine.all, driverTransaction'length);
+          
+          elsif command.all = "at" then
+            if verbosity > 1 then
+              print(cr & "Advancing simulation to time " & driverLine.all);
+            end if;
+            delay := sscanf(driverLine.all);
+            wait for delay - now;
+          
+          elsif command.all = "wait" then
+            if verbosity > 1 then
+              print(cr &"Advancing simulation of step " & driverLine.all);
+            end if;
+            delay := sscanf(driverLine.all);
+            wait for delay;
+        
+          elsif command.all = "end_sim" then
+            if verbosity > 0 then
+              print(cr & cr & "================================");
+              print("================================");
+              print("Simulation end");
+              print("================================");
+              print("================================");
+            end if;
+            assert false
+              report "Simulation end"
+              severity note;
+            stop; 
+            wait;
+           
+          else
+            driverTransaction <= pad(" ", driverTransaction'length);
+            wait for 0 ns;
+            driverTransaction <= pad(
+              command.all & " " & driverLine.all,
+              driverTransaction'length
+            );
+            if verbosity > 0 then
+              print(command.all & " " & driverLine.all);
+            end if;
           end if;
         end if;
-
       end if;
+      -- Execute one delta simulation step (no time change, but line is executed)
+      wait for 0 ns;
     end loop;
     file_close(driverFile);
 
