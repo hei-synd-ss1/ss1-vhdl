751519e7fc3410820ed48fc6efdbd91206a35ff7
Silvan Zahno <silvan.zahno@hevs.ch>
2025-03-11 07:55:41 +0000

    2025-03-11 07:55:38: Automatic Library Update with deployLibs.bash :shipit:

--- Libs/RS232/hdl/serialPortTransmitterParity_RTL.vhd
+++ Libs/RS232/hdl/serialPortTransmitterParity_RTL.vhd
@@ -34,99 +34,97 @@
 library Common;
 use Common.CommonLib.all;
 
-ARCHITECTURE RTL OF serialPortTransmitterParity IS
+ARCHITECTURE rtl OF serialPortTransmitterParity IS
 
-constant c_PARITY_BIT_NB: integer := 1;
-signal lvec_divider_counter: unsigned(requiredBitNb(g_BAUD_RATE_DIVIDE)-1 downto 0);
-signal lsig_divider_counter_reset: std_uLogic;
-signal lvec_tx_data: unsigned(g_DATA_BIT_NB-1 downto 0);
-signal lsig_send1: std_uLogic;
-signal lsig_tx_shift_enable: std_uLogic;
-signal lvec_tx_shift_reg: unsigned(g_DATA_BIT_NB + g_STOP_BIT_NB + c_PARITY_BIT_NB downto 0);
-signal lsig_tx_sending_byte: std_uLogic;
-signal lsig_tx_sending_byte_and_stop: std_uLogic;
+  -- FSM
+  type state_t is (ST_IDLE, ST_SENDING_BYTE, ST_STOP_HOLD);
+  signal lvec_state: state_t;
+  signal lsig_is_idle: std_uLogic;
+  -- What is to be sent
+  constant c_PARITY_BIT_NB: integer := 1;
+  constant c_DATA_SIZE: integer := g_DATA_BIT_NB + g_STOP_BIT_NB + c_PARITY_BIT_NB;
+  signal lvec_tx_shift_reg: unsigned(c_DATA_SIZE downto 0);
+  constant c_DATA_CNT_TARGET: unsigned(requiredBitNb(c_DATA_SIZE)-1 downto 0) := to_unsigned(c_DATA_SIZE, requiredBitNb(c_DATA_SIZE));
+  signal lvec_data_cnt: unsigned(requiredBitNb(c_DATA_SIZE)-1 downto 0);
+  -- Clock divider
+  signal lvec_divider_counter: unsigned(requiredBitNb(g_BAUD_RATE_DIVIDE)-1 downto 0);
+    -- When overflows
+  signal lsig_divider_of: std_uLogic;
 
 BEGIN
 
-  assert false report "o_txd_en not handled" severity failure;
+  -- FSM
+  fsm_proc : process(reset, clock)
+  begin
+    if reset = '1' then
+      lvec_state <= ST_IDLE;
+      lvec_tx_shift_reg <= (others => '1');
+      lvec_data_cnt <= (others => '0');
+    elsif rising_edge(clock) then
+      case lvec_state is
+        
+        when ST_IDLE =>
+          lvec_data_cnt <= (others => '0');
+          if i_send = '1' then
+            lvec_state <= ST_SENDING_BYTE;
+            -- Preload data
+            lvec_tx_shift_reg(0) <= '0';                                           -- start bit
+            if g_LSB_FIRST = '1' then
+              lvec_tx_shift_reg(g_DATA_BIT_NB downto 1) <= unsigned(i_data);       -- data
+            else
+              for i in 0 to g_DATA_BIT_NB-1 loop
+                lvec_tx_shift_reg(g_DATA_BIT_NB - i) <= i_data(i);
+              end loop;
+            end if;
+            lvec_tx_shift_reg(lvec_tx_shift_reg'high-1) <= xor unsigned(i_data);  -- parity bit
+            lvec_tx_shift_reg(lvec_tx_shift_reg'high) <= '1';                     -- stop bit
+          end if;
 
-  divide: process(reset, clock)
+        when ST_SENDING_BYTE =>
+          -- Bit clock overflows
+          if lsig_divider_of = '1' then
+            lvec_tx_shift_reg <= '1' & lvec_tx_shift_reg(lvec_tx_shift_reg'high downto 1);
+            lvec_data_cnt <= lvec_data_cnt + 1;
+            if lvec_data_cnt = c_DATA_CNT_TARGET then
+              lvec_state <= ST_STOP_HOLD;
+            end if;
+          end if;
+        
+        when ST_STOP_HOLD =>
+          if lsig_divider_of = '1' then
+            lvec_state <= ST_IDLE;
+          end if;
+          
+        when others =>
+          lvec_state <= ST_IDLE;
+
+      end case;
+    end if;
+  end process fsm_proc;
+
+  lsig_is_idle <= '1' when lvec_state = ST_IDLE else '0';
+
+  -- Clock divider
+  clk_divider_proc: process(reset, clock)
   begin
     if reset = '1' then
       lvec_divider_counter <= (others => '0');
+      lsig_divider_of <= '0';
     elsif rising_edge(clock) then
-      if lsig_divider_counter_reset = '1' then
-        lvec_divider_counter <= to_unsigned(1, lvec_divider_counter'length);
+      lsig_divider_of <= '0';
+      if lsig_is_idle = '1' or lvec_divider_counter = g_BAUD_RATE_DIVIDE - 1 then
+        lvec_divider_counter <= (others => '0');
+        lsig_divider_of <= not lsig_is_idle;
       else
         lvec_divider_counter <= lvec_divider_counter + 1;
       end if;
     end if;
-  end process divide;
+  end process clk_divider_proc;
 
-  endOfCount: process(lvec_divider_counter, lsig_send1)
-  begin
-    if lvec_divider_counter = g_BAUD_RATE_DIVIDE then
-      lsig_divider_counter_reset <= '1';
-    elsif lsig_send1 = '1' then
-      lsig_divider_counter_reset <= '1';
-    else
-      lsig_divider_counter_reset <= '0';
-    end if;
-  end process endOfCount;
+  -- Output signals
+  o_txd <= lvec_tx_shift_reg(0) when lsig_is_idle = '0' else '1';
+  o_busy <= not lsig_is_idle;
+  o_txd_en <= not lsig_is_idle;
 
-  lsig_tx_shift_enable <= lsig_divider_counter_reset;
-
-  storeData: process(reset, clock)
-  begin
-    if reset = '1' then
-      lvec_tx_data <= (others => '1');
-    elsif rising_edge(clock) then
-      if i_send = '1' then
-        lvec_tx_data <= unsigned(i_data);
-      end if;
-    end if;
-  end process storeData;
-
-  delaySend: process(reset, clock)
-  begin
-    if reset = '1' then
-      lsig_send1 <= '0';
-    elsif rising_edge(clock) then
-      lsig_send1 <= i_send;
-    end if;
-  end process delaySend;
-
-  shiftReg: process(reset, clock)
-  begin
-    if reset = '1' then
-      lvec_tx_shift_reg <= (others => '1');
-    elsif rising_edge(clock) then
-      if lsig_tx_shift_enable = '1' then
-        if lsig_send1 = '1' then
-          lvec_tx_shift_reg <= (others => '1');                             -- stop bits
-          lvec_tx_shift_reg(0) <= '0';                                      -- start bit
-          lvec_tx_shift_reg(lvec_tx_data'high+1 downto 1) <= lvec_tx_data;  -- data
-          lvec_tx_shift_reg(lvec_tx_shift_reg'high-1) <= xor lvec_tx_data;  -- parity bit
-          lvec_tx_shift_reg(lvec_tx_shift_reg'high) <= '0';                 -- end flag
-        else
-          lvec_tx_shift_reg <= shift_right(lvec_tx_shift_reg, 1);
-          lvec_tx_shift_reg(lvec_tx_shift_reg'high) <= '1';
-        end if;
-      end if;
-    end if;
-  end process shiftReg;
-
-  lsig_tx_sending_byte <= '1' when
-    (lvec_tx_shift_reg(lvec_tx_shift_reg'high downto 1) /= (lvec_tx_shift_reg'high downto 1 => '1'))
-    else '0';
-
-  lsig_tx_sending_byte_and_stop <= '1' when
-    lvec_tx_shift_reg /= (lvec_tx_shift_reg'high downto 0 => '1')
-    else '0';
-
-  o_txd <= lvec_tx_shift_reg(0) when lsig_tx_sending_byte = '1' else '1';
-  o_txd_en <= '0';
-  o_busy <= lsig_tx_sending_byte_and_stop or lsig_send1 or i_send;
-
-END ARCHITECTURE RTL;
+END ARCHITECTURE rtl;
 
